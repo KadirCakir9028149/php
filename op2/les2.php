88ba338fd2350f0afb9b647b591bce3917444802
Kadir Cakir <ka.c0825@gmail.com>
2026-03-05 13:33:16 +0100

    op 2

---
+++ op2/les2.php
@@ -0,0 +1,24 @@
+<?php
+$ineglogd = true;
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+<?php
+if ($ineglogd) {
+    echo "welkom.";
+} else {
+    echo "log eerst in.";
+}
+?>
+
+
+</body>
+</html>
