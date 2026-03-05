dc76537eb673f7d2476c6472f5fdb5f3a5c02b45
Kadir Cakir <ka.c0825@gmail.com>
2026-03-05 13:20:09 +0100

    op 1

---
+++ op1/shorthand.php
@@ -0,0 +1,8 @@
+<?php
+$naam = "kadir";
+$leeftijd = "17";
+$favorieteDier = "kat";
+?>
+
+<h1> welkom <?= $naam ?></h1>
+<p>ik ben <?= $leeftijd ?> jaar oud en mijn favoriete dier is een <?= $favorieteDier ?>.</p>
